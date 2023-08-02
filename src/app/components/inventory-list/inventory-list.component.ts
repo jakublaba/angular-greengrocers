a1f73aa9223395aa1825d4510e980b9c3eeb6998
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-08-02 14:06:43 +0200

    Extension feature: Filter store items by type (#1)
    * Rename component
    
    * Implement filtering store by item type

---
+++ src/app/components/inventory-list/inventory-list.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {GroceryService} from "../../service/grocery.service";
+import Item from "../../models/item";
+
+@Component({
+  selector: 'app-inventory-list',
+  templateUrl: './inventory-list.component.html',
+  styleUrls: ['./inventory-list.component.css']
+})
+export class InventoryListComponent {
+  @Input() groceries: Item[] | null = null;
+
+  constructor(private readonly groceryService: GroceryService) {
+  }
+}
